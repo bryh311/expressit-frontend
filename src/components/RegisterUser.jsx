99ca7102dbace6689d3ede9c90d5db753be7b776
Bryce Hill <bryh311@gmail.com>
2023-07-24 11:02:00 -0400

    css and register login

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
@@ -13,19 +13,24 @@ export default function RegisterUser(props) {
 
     function handleChange(e) {
         setUser({...user, [e.target.name]: e.target.value})
-        console.log(user)
+        // console.log(user)
     }
 
     function handleSubmit(e) {
         e.preventDefault()
         axios.post('http://localhost:8080/api/users/', user)
         .then((res) => {
+            axios.post('http://localhost:8080/api/users/login', user)
+            .then((res) => {
+                const token = res.data.access_token
+                localStorage.setItem("auth_token", token)
+            })
             setUser({
                 email: '',
                 username: '',
                 password: ''
             })
-            navigate('/')
+            // navigate('/')
         })
         .catch((err) => {
             console.log('Error in RegisterUser!')
@@ -34,14 +39,14 @@ export default function RegisterUser(props) {
 
     return (
         <>
-        <h2>Register New User</h2>
-        <div className='form-group'>
-        <form noValidate onSubmit={handleSubmit} className='form-row'>
-            <input placeholder='Email' name='email' type="text" value={user.email} onChange={handleChange}></input><br></br>
-            <input placeholder='Username' name='username' type="text" value={user.username} onChange={handleChange}></input><br></br>
-            <input placeholder='Password' name='password' type="password" value={user.password} onChange={handleChange}></input><br></br>
-            <input type='submit'></input>
-        </form>
+        <div className='container-sm border border-primary rounded p-10 mt-4'>
+            <h2>Register New User</h2>
+            <form noValidate onSubmit={handleSubmit}>
+                <input className='form-control' placeholder='Email' name='email' type="text" value={user.email} onChange={handleChange}></input><br></br>
+                <input className='form-control' placeholder='Username' name='username' type="text" value={user.username} onChange={handleChange}></input><br></br>
+                <input className='form-control' placeholder='Password' name='password' type="password" value={user.password} onChange={handleChange}></input><br></br>
+                <input className='btn btn-primary mb-3' type='submit' value="Create"></input>
+            </form>
         </div>
         </>
     )
